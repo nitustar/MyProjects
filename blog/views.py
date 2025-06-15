7d03ba8b9e317bb6500cca3f75f60ee1e3ee4232
Nitesh Kumar <nkniteshstar@gmail.com>
2025-06-15 20:46:55 +0530

    Adding blog views, template files.

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import get_object_or_404, render
 from .models import Post
 # Create your views here.
 
-def post_list(requests):
+def post_list(request):
     posts = Post.objects.all()
-    return render(requests, 'blog/post/list.html', {'posts':posts})
+    return render(request, 'blog/post/list.html', {'posts':posts})
 
-def post_details(requests, id):
+def post_detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    return render(requests, 'blog/post/detail.html', {'posts':post})
+    return render(request, 'blog/post/detail.html', {'posts':post})
