5dd198e024c3331905ad22595f81c493374d4d5d
Nitesh Kumar <nkniteshstar@gmail.com>
2025-06-29 20:30:17 +0530

    Adding Pagination and slug url in app.

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,29 @@
 from django.http import Http404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.all()
+    post_list = Post.objects.all()
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer get the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range get last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts':posts})
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year = year,
+                             publish__month = month,
+                             publish__day = day
+    )
     return render(request, 'blog/post/detail.html', {'posts':post})
