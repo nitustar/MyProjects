6550d601027a3b61211826a6f9fad012b16f35c7
Nitesh Kumar <nkniteshstar@gmail.com>
2025-07-01 22:23:01 +0530

    Adding send email code and template to this blog app.

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,37 @@
 from django.http import Http404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.views.generic import ListView
 from django.shortcuts import get_object_or_404, render
+from django.core.mail import send_mail
 from .models import Post
+from .forms import EmailPostForm
 # Create your views here.
 
-def post_list(request):
-    post_list = Post.objects.all()
-    # Pagination with 3 posts per page
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # If page_number is not an integer get the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page_number is out of range get last page of results
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts':posts})
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+# def post_list(request):
+#     post_list = Post.objects.all()
+#     # Pagination with 3 posts per page
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # If page_number is not an integer get the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page_number is out of range get last page of results
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html', {'posts':posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
@@ -27,3 +41,41 @@ def post_detail(request, year, month, day, post):
                              publish__day = day
     )
     return render(request, 'blog/post/detail.html', {'posts':post})
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(
+        Post, id = post_id,
+        status = Post.Status.PUBLISHED
+    )
+    sent = False
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        # Form fields passed validation
+        if form.is_valid():
+            cd = form.cleaned_data
+            # ... send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = (f"{cd['name']} ({cd['email']})" f"reommends you read {post.title}")
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']} \'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']]
+            )
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(
+        request, 'blog/post/share.html',
+        {
+            'post': post,
+            'form': form,
+            'sent': sent
+        }
+    )
